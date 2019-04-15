6755a20814acd6cf27577450521cfd6f88991738
Taylor Bauer <taylorbauer@ku.edu>
2019-04-15 17:07:50 -0500

    First few hours of work, wrote 17 tests and found hopefully the majority of bugs

---
+++ Test.h
@@ -0,0 +1,44 @@
+/**
+*	@file Test.h
+*	@author Taylor Bauer
+*	@date April 15, 2019
+*	@brief A header file for Test Suite class
+*/
+
+#ifndef TEST_H
+#define TEST_H
+#include <string>
+#include "LinkedListOfInts.h"
+
+class Test {
+    public:
+    Test();
+    void runTests();
+
+    private:
+    int testCount;
+    std::string testToString(bool result);
+
+    LinkedListOfInts simpleList();
+
+    bool sizeOfEmpty();
+    bool oneAddFront();
+    bool oneAddBack();
+    bool oneRemoveFront();
+    bool oneRemoveBack();
+    bool sizeSimpleList();
+    bool tooManyRemoveFronts();
+    bool tooManyRemoveBacks();
+    bool emptyAfterRemoveFronts();
+    bool emptyAfterRemoveBacks();
+    bool nonEmpty();
+    bool searchReturnsTrue();
+    bool searchReturnsFalse();
+    bool searchWithMultiples();
+    bool emptyListSearch();
+    bool emptyRemoveFront();
+    bool emptyRemoveBack();
+
+};
+
+#endif
